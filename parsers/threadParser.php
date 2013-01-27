651bdbdd6f4ba5940509feb6b38867fc74fdc4aa
Eric Wieser <wieser.eric@gmail.com>
2013-01-27 11:35:57 +0000

    Made things classy

--- parsers/threadParser.php
+++ parsers/threadParser.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once "includes/classes.php";
+
 // nodeValue strips out newlines, this function keeps HTML intact
 function innerXML($node) { 
     $doc  = $node->ownerDocument; 
@@ -31,49 +34,49 @@ else
         $holder = $holder->childNodes;
         foreach($holder as $post) {
             if (($post->childNodes->length == 3) && ($post->getElementsByTagName('td')->length != 0)) {
-                $authorSect = $post->childNodes->item(0)->childNodes->item(0);
                 $postSect =  $post->childNodes->item(1)->childNodes->item(0);
-                //Really weird hax stoof
-                $authorName = trim(substr($authorSect->childNodes->item(0)->nodeValue,2));
-                $authorURL = "http://roblox.com/User.aspx?username=" . $authorName;
-                $authorImg = "http://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&Format=Png&username=" . $authorName;
-                $authorIcon = $authorSect->getElementsByTagName('img')->item(0)->getAttribute('src');
-                //Figure out if online or offline
 
-                $isOnline = $authorIcon == "/Forum/skins/default/images/user_IsOnline.gif";
-                $authorDate = trim(substr($authorSect->childNodes->item(2)->nodeValue,8));
+                $authorSect = $post->childNodes->item(0)->childNodes->item(0);
+                $authorIcon = $authorSect->getElementsByTagName('img')->item(0)->getAttribute('src');
+
+                $post = new Post();
+
+                //Really weird hax stoof
+                $post->author = new User();
+                $post->author->name = trim(substr($authorSect->childNodes->item(0)->nodeValue,2));
+                $post->author->online = $authorIcon == "/Forum/skins/default/images/user_IsOnline.gif";
+                $post->author->joinDate = trim(substr($authorSect->childNodes->item(2)->nodeValue,8));
+
                 // Hax to fix problem with displaying info if they have an img under name
-                if ($authorDate == "") {
+                if ($post->author->joinDate == "") {
                     // Figure out if mod
                     $modIndic = $authorSect->getElementsByTagName('img')->item(3)->getAttribute('src');
                     if (substr($modIndic,1,36) == "Forum/skins/default/images/users_top" && $authorSect->getElementsByTagName('img')->length == 5) {
                         // They're a mod and a top poster
-                        $isMod = true;
-                        $authorDate = substr($authorSect->childNodes->item(4)->nodeValue,8);
-                        $authorPosts = substr($authorSect->childNodes->item(5)->nodeValue,13);
+                        $post->author->isMod = true;
+                        $post->author->joinDate = trim(substr($authorSect->childNodes->item(4)->nodeValue,8));
+                        $post->author->postCount = trim(substr($authorSect->childNodes->item(5)->nodeValue,13));
                     }
                     if ($modIndic == "/Forum/skins/default/images/users_moderator.gif") {
                         // If they're just a mod
-                        $isMod = true;
-                        $authorDate = substr($authorSect->childNodes->item(3)->nodeValue,8);
-                        $authorPosts = substr($authorSect->childNodes->item(4)->nodeValue,13);
+                        $post->author->isMod = true;
+                        $post->author->joinDate = trim(substr($authorSect->childNodes->item(3)->nodeValue,8));
+                        $post->author->postCount = trim(substr($authorSect->childNodes->item(4)->nodeValue,13));
                     }
                 }
                 else {
-                    $authorPosts = trim(substr($authorSect->childNodes->item(3)->nodeValue,13));
+                    $post->author->postCount = trim(substr($authorSect->childNodes->item(3)->nodeValue,13));
                 }
                 // line below doesn't work
                 //$authorGroup = $authorSect->childNodes->item(4)->getElementsByTagName('a')->item(0)->getAttribute('src');
                 // echo $authorGroup;
-                $postTitle = $postSect->childNodes->item(0)->childNodes->item(0);
-                $postTopic = $postTitle->childNodes->item(0)->nodeValue;
-                $postDate = $postTitle->childNodes->item(3)->nodeValue;
-                $postContents = innerXML($postSect->childNodes->item(1));
+                $postTitleSect = $postSect->childNodes->item(0)->childNodes->item(0);
+                $post->title = $postTitleSect->childNodes->item(0)->nodeValue;
+                $post->date = $postTitleSect->childNodes->item(3)->nodeValue;
+                $post->content = innerXML($postSect->childNodes->item(1));
 
                 // include our template to separate parsing from presentation
                 include "templates/post.php";
-
-                $isMod = false; // Reset mod status so everyone doesn't get the icon
             }
         }
     }
--- templates/post.php
+++ templates/post.php
@@ -1,25 +1,24 @@
-<li data-role='list-divider'><?= $postTopic ?></li>
+<li data-role='list-divider'><?= $post->title ?></li>
 <li data-theme='d'>
     <div data-role='collapsible' data-icon='delete' data-mini='true'>
         <h5>
-<?php if($isMod): ?>
+<?php if($post->author->isMod): ?>
             <img src='includes/mod.gif' alt='MOD' />
 <?php endif ?>
-<?php if($isOnline): ?>
-            <font color='green'><?= $authorName ?></font>
-<?php else: ?>
-            <font color='red'><?= $authorName ?></font>
-<?php endif ?>
+            <font color='<?php if($post->author->online): ?>green<?php else: ?>red<?php endif ?>'>
+                <?= $post->author->name ?>
+
+            </font>
         </h5>
         <p>
-            <img src='<?= $authorImg ?>' alt='<?= $authorName ?>' /><br>
-            Joined <b><?= $authorDate ?></b><br>
-            Total Posts <b><?= $authorPosts ?></b><br>
-            <a href='<?= $authorURL ?>' target='_blank'>View Profile &#187;</a>
+            <img src='<?= $post->author->img ?>' alt='<?= $post->author->name ?>' /><br>
+            Joined <b><?= $post->author->joinDate ?></b><br>
+            Total Posts <b><?= $post->author->postCount ?></b><br>
+            <a href='<?= $post->author->url ?>' target='_blank'>View Profile &#187;</a>
         </p>
     </div>
     <p style='word-break: break-all;'>
-        <?= $postContents ?><br><br>
-        <b>Posted</b> <?= $postDate ?>
+        <?= $post->content ?><br><br>
+        <b>Posted</b> <?= $post->date ?>
     </p>
 </li>
