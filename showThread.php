d4c6a9e6cafb5f27be6b6d9f8b353edb02603769
Eric Wieser <wieser.eric@gmail.com>
2013-02-03 19:12:21 +0000

    Pagination now handled completely within showThread. Somewhat bodged.

--- showThread.php
+++ showThread.php
@@ -14,8 +14,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 	$thread->pageNum = $pageNum;
 
 	templatePage("$name | ROBLOX Forums", function() {
-		global $thread;
-		global $name, $desc, $id, $pageNum, $page;
+		global $thread, $errored;
 	?>
 		<a href='<?= htmlspecialchars($thread->url) ?>' target='_blank' data-role='button'>
 			Show Original
@@ -29,9 +28,21 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 		endforeach ?>
 		</ul>
 <?php
-	}, function() {
-		global $name, $desc, $id, $pageNum, $page;
-		include('includes/paginationFooter.php');
-	});
+	}, $thread->totalPages > 1 ? function() {
+		global $thread;
+?>
+	<div style = "text-align: center;" data-role="controlgroup" data-type="horizontal">
+<?php if ($thread->pageNum != 1): ?>
+		<a href='?page=<?= $thread->pageNum - 1 ?>'
+		   data-theme='e' data-role='button' data-icon='arrow-l' data-iconpos='notext'>Previous</a>
+<?php endif ?>
+		<a href='#' data-role='button' data-theme='c'><?= $thread->pageNum ?> of <?= $thread->totalPages?></a>
+<?php if ($thread->pageNum != $thread->totalPages): ?>
+		<a href='?page=<?= $thread->pageNum + 1 ?>'
+		   data-theme='e' data-role='button' data-icon='arrow-r' data-iconpos='notext'>Next</a>
+<?php endif ?>
+	</div>
+<?php
+	} : NULL);
 }
 ?>
