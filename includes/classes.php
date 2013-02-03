3e3f205db50a0026af46921b8d7b18b3361473e2
Eric Wieser <wieser.eric@gmail.com>
2013-02-03 14:53:35 +0000

    Moved the one-instance-only and parsing logic into the User class.

--- includes/classes.php
+++ includes/classes.php
@@ -31,12 +31,17 @@ class GroupInfo extends EnhancedObject {
 }
 
 class User extends EnhancedObject {
+	private static $_instances = array();
+
+	# Properties
 	public $name;
 	public $joinDate;
 	public $postCount;
 	public $online;
 	public $isMod;
 	public $groupInfo;
+
+	private $_fullyLoaded;
 
 	public function getUrl() {
 		return "http://roblox.com/User.aspx?username=" . $this->name;
@@ -45,15 +50,75 @@ class User extends EnhancedObject {
 		return "http://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&Format=Png&username=" . $this->name;
 	}
 
-	public static function getGroupInfo($users) {
-		$url = "http://www.roblox.com/Groups/GetPrimaryGroupInfo.ashx?users=".implode(',', $users);
-		$data = file_get_contents($url);
+	/**
+	 * Private constructor to create a user with a given name
+	 */
+	private function __construct($name) {
+		$this->name = $name;
+		self::$_instances[$name] = $this;
+	}
+	/**
+	 * Get either a new or existing user object based on a name
+	 */
+	public function byName($name) {
+		$u = @self::$_instances[$name] or $u = new User($name);
+		return $u;
+	}
 
-		$parsed = array();
-		foreach(json_decode($data) as $user => $entry) {
-			$parsed[$user] = GroupInfo::fromJsonEntry($entry);
+	/**
+	 * Parses an author object out of the corresponding author box in a thread list
+	 */
+	public static function fromPostAuthorInfo($domElement) {
+		$authorName = trim(substr($domElement->childNodes->item(0)->nodeValue,2));
+		$author = User::byName($authorName);
+
+		if(!$author->_fullyLoaded) {
+			$authorIcon = $domElement->getElementsByTagName('img')->item(0)->getAttribute('src');
+			// Assigning author values
+			$author->online = $authorIcon == "/Forum/skins/default/images/user_IsOnline.gif";
+			$author->joinDate = trim(substr($domElement->childNodes->item(2)->nodeValue,8));
+
+			// Figure out if the poster is a mod/top poster/both and adjust information accordingly
+			if ($author->joinDate == "") {
+				// Figure out if mod
+				$modIndic = $domElement->getElementsByTagName('img')->item(3)->getAttribute('src');
+				if (substr($modIndic,1,36) == "Forum/skins/default/images/users_top" && $domElement->getElementsByTagName('img')->length == 5) {
+					// They're a mod and a top poster
+					$author->isMod = true;
+					$author->joinDate = trim(substr($domElement->childNodes->item(4)->nodeValue,8));
+					$author->postCount = trim(substr($domElement->childNodes->item(5)->nodeValue,13));
+				}
+				if ($modIndic == "/Forum/skins/default/images/users_moderator.gif") {
+					// If they're just a mod
+					$author->isMod = true;
+					$author->joinDate = trim(substr($domElement->childNodes->item(3)->nodeValue,8));
+					$author->postCount = trim(substr($domElement->childNodes->item(4)->nodeValue,13));
+				}
+			}
+			else {
+				$author->postCount = trim(substr($domElement->childNodes->item(3)->nodeValue,13));
+			}
+			$author->_fullyLoaded = true;
 		}
-		return $parsed;
+		return $author;
+	}
+
+	/**
+	 * Populates every author object in existance with their group info
+	 */
+	public static function populateGroupInfo() {
+		# Get a list of the names of unpopulated users
+		$names = array();
+		foreach(self::$_instances as $name => $user)
+			if(!$user->groupInfo)
+				$names[] = $name;
+
+		$url = "http://www.roblox.com/Groups/GetPrimaryGroupInfo.ashx?users=".implode(',', $names);
+		$data = file_get_contents($url);
+		$parsed = array();
+		foreach(json_decode($data) as $username => $entry) {
+			$_instances[$username]->groupInfo = GroupInfo::fromJsonEntry($entry);
+		}
 	}
 }
 
--- parsers/threadParser.php
+++ parsers/threadParser.php
@@ -23,7 +23,6 @@ class Thread extends EnhancedObject {
 	public function loadPosts() {
 		global $errored, $page;
 		$posts = array();
-		$authors = array();
 
 		// Setup
 		$html = @file_get_contents($this->url);
@@ -53,44 +52,10 @@ class Thread extends EnhancedObject {
 				$postSect =  $post->childNodes->item(1)->childNodes->item(0);
 
 				$authorSect = $post->childNodes->item(0)->childNodes->item(0);
-				$authorIcon = $authorSect->getElementsByTagName('img')->item(0)->getAttribute('src');
 
 				$post = new Post();
-				$authorName = trim(substr($authorSect->childNodes->item(0)->nodeValue,2));
 
-				if(!isset($authors[$authorName])) {
-					// Assigning author values
-					$post->author = new User();
-					$post->author->name = $authorName;
-					$post->author->online = $authorIcon == "/Forum/skins/default/images/user_IsOnline.gif";
-					$post->author->joinDate = trim(substr($authorSect->childNodes->item(2)->nodeValue,8));
-
-					// Figure out if the poster is a mod/top poster/both and adjust information accordingly
-					if ($post->author->joinDate == "") {
-						// Figure out if mod
-						$modIndic = $authorSect->getElementsByTagName('img')->item(3)->getAttribute('src');
-						if (substr($modIndic,1,36) == "Forum/skins/default/images/users_top" && $authorSect->getElementsByTagName('img')->length == 5) {
-							// They're a mod and a top poster
-							$post->author->isMod = true;
-							$post->author->joinDate = trim(substr($authorSect->childNodes->item(4)->nodeValue,8));
-							$post->author->postCount = trim(substr($authorSect->childNodes->item(5)->nodeValue,13));
-						}
-						if ($modIndic == "/Forum/skins/default/images/users_moderator.gif") {
-							// If they're just a mod
-							$post->author->isMod = true;
-							$post->author->joinDate = trim(substr($authorSect->childNodes->item(3)->nodeValue,8));
-							$post->author->postCount = trim(substr($authorSect->childNodes->item(4)->nodeValue,13));
-						}
-					}
-					else {
-						$post->author->postCount = trim(substr($authorSect->childNodes->item(3)->nodeValue,13));
-					}
-					$authors[$authorName] = $post->author;
-				}
-				else {
-					// reuse old object
-					$post->author = $authors[$authorName];
-				}
+				$post->author = User::fromPostAuthorInfo($authorSect);
 
 				$postTitleSect = $postSect->childNodes->item(0)->childNodes->item(0);
 				$post->title = trim($postTitleSect->childNodes->item(0)->nodeValue);
@@ -102,11 +67,7 @@ class Thread extends EnhancedObject {
 			}
 		}
 
-		$groups = User::getGroupInfo(array_keys($authors));
-
-		foreach($authors as $name => $data) {
-			$data->groupInfo = @$groups[$name];
-		}
+		User::populateGroupInfo();
 
 		return $posts;
 	}
