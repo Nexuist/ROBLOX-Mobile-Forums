2121f5eceb4dbc568aaa84aaad602bbe342220ef
Eric Wieser <wieser.eric@gmail.com>
2013-02-09 15:14:54 +0000

    Updated jQuery + jQuery mobile

--- templates/page.php
+++ templates/page.php
@@ -8,8 +8,8 @@ function templatePage($title, $makeContent, $makeFooter = NULL) {
 	<title><?php echo $title ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset='utf-8' />
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
-	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile-1.3.0-rc.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 	<script>
 	$(document).bind("mobileinit", function(){
 		$.extend($.mobile, {
@@ -17,7 +17,7 @@ function templatePage($title, $makeContent, $makeFooter = NULL) {
 		});
 	});
 	</script>
-	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile-1.3.0-rc.1.min.js"></script>
 	<link rel="shortcut icon" href="http://www.imgbomb.com/i/d15/5zfem.png" />
 	<style type = "text/css">
 		a {
